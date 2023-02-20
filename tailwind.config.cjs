4f584df6f4f9a91014693d40ed3d78ff4e2fad5c
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-02-20 11:06:48 +0100

    add background color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#F9F7F7'
+      }
+    }
   },
   plugins: []
 }
